3a374dad5e3e12eb482cb0c0f4f1725084f63c73
Gabriel Kopper <41166074+gckopper@users.noreply.github.com>
2025-06-16 00:05:25 -0300

    feat: implementando o repositorio de usuario e arrumanto os mappers

--- src/main/java/com/example/todo/infrastructure/model/TaskMapper.java
+++ src/main/java/com/example/todo/infrastructure/model/TaskMapper.java
@@ -1,27 +1,24 @@
 package com.example.todo.infrastructure.model;
 
 import com.example.todo.domain.model.Task;
-import com.example.todo.domain.repository.UserRepositoryPort;
 
 public class TaskMapper {
-    private UserRepositoryPort userRepository;
-
-    public Task toDomain(TaskDto taskDto) {
+    public static Task toDomain(TaskDto taskDto) {
         return Task.builder()
             .id(taskDto.getId())
             .title(taskDto.getTitle())
             .description(taskDto.getDescription())
             .status(taskDto.getStatus())
-            .assignedToUser(userRepository.findById(taskDto.getAssignedUserId()).orElseThrow())
-            .owner(userRepository.findById(taskDto.getOwnerUserId()).orElseThrow())
+            .assignedToUser(UserMapper.toDomain(taskDto.getAssignedUser()))
+            .owner(UserMapper.toDomain(taskDto.getOwnerUser()))
             .build();
     }
 
     public TaskDto toDto(Task task) {
         return TaskDto.builder()
             .id(task.getId())
-            .ownerUserId(task.getOwner().getId())
-            .assignedUserId(task.getAssignedToUser().getId())
+            .ownerUser(UserMapper.toDto(task.getOwner()))
+            .assignedUser(UserMapper.toDto(task.getAssignedToUser()))
             .title(task.getTitle())
             .description(task.getDescription())
             .status(task.getStatus())
--- src/main/java/com/example/todo/infrastructure/repository/JpaUserRepository.java
+++ src/main/java/com/example/todo/infrastructure/repository/JpaUserRepository.java
@@ -1,8 +1,11 @@
 package com.example.todo.infrastructure.repository;
 
-import com.example.todo.domain.repository.UserRepositoryPort;
+import com.example.todo.infrastructure.model.UserDto;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public class JpaUserRepository implements UserRepositoryPort {
+public interface JpaUserRepository extends JpaRepository<UserDto, UUID> {
 }
