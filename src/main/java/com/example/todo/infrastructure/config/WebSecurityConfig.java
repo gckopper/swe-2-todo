993323423e7f1be45f0009e0fe62c2a9a067fd5e
Gabriel Kopper <41166074+gckopper@users.noreply.github.com>
2025-06-18 18:13:11 -0300

    removendo a autenticacao para testes

---
+++ src/main/java/com/example/todo/infrastructure/config/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.todo.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(authManager ->
+            authManager.anyRequest().permitAll()).build();
+    }
+}
